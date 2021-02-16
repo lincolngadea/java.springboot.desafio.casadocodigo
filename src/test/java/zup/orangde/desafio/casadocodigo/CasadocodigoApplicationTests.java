1e23000a640bfa8122a5f413c3068b8befaff83f
Lincoln Gadea <lincoln@glinc.com.br>
2021-02-16 10:16:30 -0300

    chore: cria persistência com o banco e insere primeiros dados na tabela Autor

--- src/test/java/zup/orangde/desafio/casadocodigo/CasadocodigoApplicationTests.java
+++ src/test/java/zup/orangde/desafio/casadocodigo/CasadocodigoApplicationTests.java
@@ -1,13 +1,12 @@
 package zup.orangde.desafio.casadocodigo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CasadocodigoApplicationTests {
+//
+//@SpringBootTest
+//class CasadocodigoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
 
-	@Test
-	void contextLoads() {
-	}
 
-}
