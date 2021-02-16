a0fa481ebe4b6807ba0bf6a52b91909ee4fc6d1f
Lincoln Gadea <lincoln@glinc.com.br>
2021-02-16 16:20:11 -0300

    chore: cria GET e POST usando entityManager com Controller

--- src/main/java/zup/orangde/desafio/casadocodigo/domain/repository/AutorRepository.java
+++ src/main/java/zup/orangde/desafio/casadocodigo/domain/repository/AutorRepository.java
@@ -1,9 +1,32 @@
 package zup.orangde.desafio.casadocodigo.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import zup.orangde.desafio.casadocodigo.domain.entity.Autor;
 
+import javax.persistence.EntityManager;
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
-public interface AutorRepository extends JpaRepository<Autor, Long> {
+public class AutorRepository {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional
+    public Autor save(Autor autor){
+
+        LocalDate date = LocalDate.now();
+        autor.setRegistrationTime(date);
+
+        entityManager.persist(autor);
+        return autor;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Autor> autorList(){
+        return entityManager.createQuery("from Autor", Autor.class).getResultList();
+    }
 }
--- src/main/java/zup/orangde/desafio/casadocodigo/rest/controller/AutorController.java
+++ src/main/java/zup/orangde/desafio/casadocodigo/rest/controller/AutorController.java
@@ -2,41 +2,30 @@ package zup.orangde.desafio.casadocodigo.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import zup.orangde.desafio.casadocodigo.domain.entity.Autor;
 import zup.orangde.desafio.casadocodigo.domain.repository.AutorRepository;
 
-import java.time.LocalDate;
-import java.util.Optional;
+import java.util.List;
 
-@Controller
+@RestController
 public class AutorController {
 
     @Autowired
     private AutorRepository autorRepository;
 
 
-    @GetMapping("/api/autor/{id}")
-    @ResponseBody
-    public ResponseEntity getAutorById(@PathVariable Long id) {
-        Optional<Autor> autorById = autorRepository.findById(id);
-
-        if (autorById.isPresent()) {
-            return ResponseEntity.ok(autorById.get());
-        }
-        return ResponseEntity.notFound().build();
-    }
-
     @PostMapping("/api/autor")
     @ResponseBody
-    public ResponseEntity saveAutor(@RequestBody Autor autor){
-
-        LocalDate date = LocalDate.now();
-        autor.setRegistrationTime(date);
-
+    public ResponseEntity autorCreate(@RequestBody Autor autor){
         Autor saveAutor = autorRepository.save(autor);
         return ResponseEntity.ok(saveAutor);
     }
 
+    @GetMapping("/api/autor/")
+    @ResponseBody
+    public ResponseEntity findAutor(){
+        List<Autor> autors = autorRepository.autorList();
+        return ResponseEntity.ok(autors);
+    }
 }
